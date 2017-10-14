59bc50b84576221f96b41e22d6f558942cf5084d
Jacob Laurenti <jlaurenti@Jacobs-MacBook-Pro.local>
2017-10-14 16:22:53 -0400

    Finished Basic Setup

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+(function() {
+  function config($locationProvider, $stateProvider) {
+  $locationProvider
+      .html5Mode({
+          enabled: true,
+          requireBase: false
+       });
+
+  $stateProvider
+      .state('home', {
+          url: '/',
+          controller: 'HomeCtrl as home',
+          templateUrl: '/templates/home.html'
+      });
+}
+
+    angular
+        .module('chat-app-6aa99', ['ui.router', 'firebase'])
+        .config(config);
+})();
---
+++ app/scripts/controllers/HomeCtrl.js
@@ -0,0 +1,8 @@
+(function() {
+    function HomeCtrl() {
+    }
+
+    angular
+        .module('chat-app-6aa99')
+        .controller('HomeCtrl', [HomeCtrl]);
+})();
